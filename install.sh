495e128a1710f87e476fd6018218ea9b03f42868
Verantor <56546640+Verantor@users.noreply.github.com>
2024-10-19 11:25:21 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+curl https://raw.githubusercontent.com/Verantor/nixfiles/refs/heads/main/hosts/main/disko.nix -o /tmp/disko-config.nix
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko-config.nix
+sudo nixos-generate-config --no-filesystems --root /mnt
+cd /mnt/etc/nixos/
+git clone https://github.com/Verantor/nixfiles
+cd nixfiles/
+sudo nixos-install --root /mnt --flake '/mnt/etc/nixos/nixfiles#main'
