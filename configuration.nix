f649b98e8c62a1125e73fe68336ffd5d4c80de47
Verantor <56546640+Verantor@users.noreply.github.com>
2024-10-19 14:05:41 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+./disko.nix
     ];
 boot.kernelPackages = pkgs.linuxPackages_latest;
             boot.supportedFilesystems = [ "bcachefs" ];
