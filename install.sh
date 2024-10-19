05f6ef59251fda7c0f48bcb1d632aa1887d32a32
Verantor <56546640+Verantor@users.noreply.github.com>
2024-10-19 11:33:22 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
-curl https://raw.githubusercontent.com/Verantor/nixfiles/refs/heads/main/hosts/main/disko.nix -o /tmp/disko-config.nix
+curl https://raw.githubusercontent.com/Verantor/NixDrop/refs/heads/main/disko.nix -o /tmp/disko-config.nix
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko-config.nix
 sudo nixos-generate-config --no-filesystems --root /mnt
+sleep 5s
 cd /mnt/etc/nixos/
-git clone https://github.com/Verantor/nixfiles
-cd nixfiles/
-sudo nixos-install --root /mnt --flake '/mnt/etc/nixos/nixfiles#main'
+git clone https://github.com/Verantor/NixDrop/
+sleep 5s
+cd NixDrop/
+sudo nixos-install --root /mnt --flake '/mnt/etc/nixos/NixDrop#nixos'
